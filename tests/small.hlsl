5f80dc909c58b29e9c4c2d193e21c5b5bf7e0772
Will Usher <will@willusher.io>
2022-01-30 16:57:30 -0800

    test more params types, exprs

--- tests/small.hlsl
+++ tests/small.hlsl
@@ -5,9 +5,10 @@ struct SceneParams {
 };
 */
 
-//in SceneParams scene;
+// in SceneParams scene;
 RWTexture2D<float4> scene_image : register(u0, space0);
-cbuffer scene_cbv : register(b0, space0) {
+cbuffer scene_cbv : register(b0, space0)
+{
     float scene_test_constant;
 }
 
@@ -15,16 +16,25 @@ struct RayGenParams {
     float4 color;
 };
 
-cbuffer RayGen_params_cbv : register(b1, space0) {
+cbuffer RayGen_params_cbv : register(b1, space0)
+{
     float4 RayGen_params_color;
 }
 
-[shader("raygeneration")]
-void RayGen()
+cbuffer scale_factor_cbv : register(b2, space0)
 {
-    uint2 pixel = DispatchRaysIndex().xy;
-    float4 c;
-    c = RayGen_params_color;
-    scene_image[pixel] = RayGen_params_color;
+    float scale_factor;
+}
+
+[shader("raygeneration")]
+void RayGen() {
+    RayGenParams params;
+    params.color = RayGen_params_color;
+    {
+        uint2 pixel = DispatchRaysIndex().xy;
+        float4 c;
+        c = params.color;
+        scene_image[pixel] = params.color * scale_factor;
+    }
 }
 
