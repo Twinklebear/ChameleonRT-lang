22b70a2de8fe7854a2b23875ba68f3a6c54352f7
Will Usher <will.usher@hey.com>
2022-03-01 22:02:23 -0800

    Pull in dx utils from ChameleonRT to start trying to run hlsl output

---
+++ renderer/main.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <sstream>
+#include <vector>
+#include <SDL.h>
+#include "display/display.h"
+#include "display/imgui_impl_sdl.h"
+#include "dx12_utils.h"
+#include "dxdisplay.h"
+#include "imgui.h"
+#include "util.h"
+
+int win_width = 1280;
+int win_height = 720;
+
+void run_app(SDL_Window *window);
+
+int main(int argc, const char **argv)
+{
+    const std::vector<std::string> args(argv, argv + argc);
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Failed to init SDL: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    const uint32_t window_flags = 0;  // SDL_WINDOW_RESIZABLE;
+    // TODO: for vulkan we need vulkan flags here,
+    // for CPU/OptiX backends using OpenGL display we need GL flags
+
+    SDL_Window *window = SDL_CreateWindow("ChameleonRT-Language Test App",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          win_width,
+                                          win_height,
+                                          window_flags);
+
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_Init(window);
+
+    run_app(window);
+
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
+glm::vec2 transform_mouse(glm::vec2 in)
+{
+    return glm::vec2(in.x * 2.f / win_width - 1.f, 1.f - 2.f * in.y / win_height);
+}
+
+void run_app(SDL_Window *window)
+{
+    auto display = std::make_shared<DXDisplay>(window);
+    display->resize(win_width, win_height);
+
+    auto render_target = dxr::Texture2D::device(display->device.Get(),
+                                                glm::uvec2(win_width, win_height),
+                                                D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
+                                                DXGI_FORMAT_R8G8B8A8_UNORM,
+                                                D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    bool done = false;
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) {
+                done = true;
+            }
+            if (!io.WantCaptureKeyboard && event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    done = true;
+                }
+            }
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE &&
+                event.window.windowID == SDL_GetWindowID(window)) {
+                done = true;
+            }
+            if (event.type == SDL_WINDOWEVENT &&
+                event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                win_width = event.window.data1;
+                win_height = event.window.data2;
+                io.DisplaySize.x = win_width;
+                io.DisplaySize.y = win_height;
+
+                // TODO resizing
+            }
+        }
+
+        display->new_frame();
+
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
+
+        ImGui::Begin("ChameleonRT Lang Demo app");
+
+        ImGui::End();
+        ImGui::Render();
+
+        display->display_native(render_target);
+    }
+}
---
+++ runtime/small.hlsl
@@ -0,0 +1,39 @@
+// CRTL HLSL Output
+struct SceneParams {
+RWTexture2D<float4> image;
+float test_constant;
+};
+
+RWTexture2D<float4> scene_image : register(u0, space0);
+cbuffer scene_test_constant_cbv : register(b0, space0) {
+	float scene_test_constant;
+}
+
+struct RayGenParams {
+float4 color;
+StructuredBuffer<float4> data;
+};
+
+StructuredBuffer<float4> RayGen_params_data : register(t0, space0);
+cbuffer RayGen_params_cbv : register(b1, space0){
+	float4 RayGen_params_color;
+}
+
+cbuffer RayGen_scale_factor_cbv : register(b2, space0) {
+	float RayGen_scale_factor;
+}
+
+[shader("raygeneration")]
+void RayGen()
+{
+RayGenParams RayGen_params;
+RayGen_params.color = RayGen_params_color;
+RayGen_params.data = RayGen_params_data;
+{
+uint2 pixel = DispatchRaysIndex().xy;
+float4 c;
+c = RayGen_params.color * scene_test_constant + RayGen_params.data[0];
+scene_image[pixel] = RayGen_params.color * RayGen_scale_factor;
+}
+}
+
