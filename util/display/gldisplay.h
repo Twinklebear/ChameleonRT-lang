98abf133eefc83c79dc3bf30e292411a0f357977
Will Usher <will.usher@hey.com>
2022-03-01 22:01:49 -0800

    add some of util from chameleonrt

---
+++ util/display/gldisplay.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <SDL.h>
+#include "display.h"
+#include "glad/glad.h"
+#include "shader.h"
+#include <glm/glm.hpp>
+
+struct GLDisplay : Display {
+    SDL_Window *window;
+    SDL_GLContext gl_context;
+    GLuint render_texture;
+    GLuint vao;
+    std::unique_ptr<Shader> display_render;
+    glm::uvec2 fb_dims;
+
+    GLDisplay(SDL_Window *window);
+
+    ~GLDisplay() override;
+
+    std::string gpu_brand() override;
+
+    std::string name() override;
+
+    void resize(const int fb_width, const int fb_height) override;
+
+    void new_frame() override;
+
+    //void display(RenderBackend *renderer) override;
+
+    void display_native(const GLuint img);
+};
+
+#if 0
+struct GLNativeRenderer : RenderBackend {
+    GLuint gl_display_texture = -1;
+};
+#endif
