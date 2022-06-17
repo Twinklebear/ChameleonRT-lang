dfececf42b2e5df70f52d96c2718495fb924598f
Will Usher <will@willusher.io>
2022-06-16 21:18:06 -0700

    putting down some notes

--- src/crtl/rhi/shader_entry_point.h
+++ src/crtl/rhi/shader_entry_point.h
@@ -2,7 +2,15 @@
 
 namespace crtl {
 class ShaderEntryPoint {
-    // TODO: Should have the name and ref to compiled parameter binding info
-    // along with parameter size, etc. so we can set params up
+public:
+    ShaderEntryPoint() = default;
+
+    virtual ~ShaderEntryPoint() = default;
+
+    // TODO: An API to get the layout for the parameters.
+    // Some parameters will be pushed up to be global scope by necessity,
+    // e.g. we cannot pass textures through the SBT
+    // Alternatively: CRTL can enforce the same restriction on apps, textures and samplers
+    // cannot be passed in the SBT. That would be easiest for a first prototype
 };
 }
--- src/crtl/rhi/shader_library.h
+++ src/crtl/rhi/shader_library.h
@@ -3,13 +3,20 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include "shader_entry_point.h"
 
 namespace crtl {
+
 class ShaderLibrary {
 public:
     ShaderLibrary() = default;
-    ShaderLibrary(const std::string &shader_string);
 
-    // ShaderEntryPoint get_entry_point(const std::string &name);
+    virtual ~ShaderLibrary() = default;
+
+    // TODO: Maybe a free-function that takes the shader lib shared ptr to keep it alive?
+    // May need to ref data from it for setting params that are pushed to be globals
+    virtual std::shared_ptr<ShaderEntryPoint> entry_point(const std::string &name) = 0;
+
+    // TODO: Here API to get global param layout
 };
 }
